9a9a3c59523a6ed1ab0b3e138f2875392d803d5b
Suvi Tiwary <suvikumari345@gmail.com>
2026-09-17 01:37:52 +0530

    1

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage , AIMessage , ToolMessage
+from langchain.tools import tool
+from tavily import TavilyClient
+import os
+import requests
 
 load_dotenv()
+
+@tool
+def get_weather(city:str)->str:
+    """ 
+    Get weather of the particular city 
+    """
+    weather_api= os.getenv("WEATHER_API_KEY")
+    response = requests.get(weather_api)
+    data = response.json()
+    print(data)
+   
+
+get_weather.invoke("what is the weather of Raxaul")
